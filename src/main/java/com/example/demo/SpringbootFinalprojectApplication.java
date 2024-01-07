12b44a985f21f24f68c416ea46fcac3396b5846b
yahya azzouz <yahya.azzouz@etu.uae.ac.ma>
2024-01-07 02:58:09 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootFinalprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFinalprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFinalprojectApplication.class, args);
+	}
+
+}
